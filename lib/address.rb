dc3a842e4bb27017d7671ed4782ba17487ba23d8
Chad Molenda <cmolenda@chads-mbp.attlocal.net>
2017-11-16 13:10:03 -0800

    Add Gelocation Resolution to Address

--- lib/address.rb
+++ lib/address.rb
@@ -1,5 +1,39 @@
 require_relative 'geocoding'
 
 class Address
-  attr_accessor :lat, :lng, :full_address
+  attr_reader :lat, :lng, :full_address
+
+  def initialize(lat: nil, lng: nil, full_address: nil)
+    @lat = lat
+    @lng = lng
+    @full_address = full_address
+
+    # NOTE: For the sake of this interview application
+    # there doesn't seem to be any benefit to delaying the
+    # geocoding. By calculating it up front, we avoid
+    # an object with state
+
+    # geocode! unless geocoded?
+    # reverse_geocode! unless reverse_geocoded?
+  end
+
+  def geocoded?
+    !(lat.nil? || lng.nil?)
+  end
+
+  def reverse_geocoded?
+    !full_address.nil?
+  end
+
+  def geocode!
+    result = Geocoder.search(full_address)&.first
+    @lat = result&.latitude
+    @lng = result&.longitude
+    self
+  end
+
+  def reverse_geocode!
+    @full_address = Geocoder.address [lat, lng]
+    self
+  end
 end
--- spec/lib/address_spec.rb
+++ spec/lib/address_spec.rb
@@ -1,57 +1,108 @@
-RSpec.describe Address do
-  let(:full_address) { '1600 Pennsylvania Avenue NW Washington, D.C. 20500 U.S.' }
-  let(:lat) { 40.181306 }
-  let(:lng) { -80.265949 }
+# NOTE: The original spec mixed the lat,lng and address for a Washington DC address
+# and Washington PA address. I've consolidated them into one set of lat,lng and address
+# for the entire spec.
 
-  subject(:address) { described_class.new }
+RSpec.describe Address do
+  let(:lat) { 38.8978707 }
+  let(:lng) { -77.0370666 }
+  let(:full_address) { '1600 Pennsylvania Avenue NW, Washington, DC 20500, USA' }
+  let(:geo_resp_params) {
+    # Keys MUST be strings
+    [
+      {
+        'latitude'     => lat,
+        'longitude'    => lng,
+        'address'      => full_address,
+        'state'        => 'District of Columbia',
+        'state_code'   => 'DC',
+        'country'      => 'United States',
+        'country_code' => 'US'
+      }
+    ]
+  }
+
+  subject(:address) { described_class.new lat: lat, lng: lng, full_address: full_address }
+
+  around do |example|
+    Geocoder::Lookup::Test.add_stub full_address, geo_resp_params
+    Geocoder::Lookup::Test.add_stub [lat, lng], geo_resp_params
+
+    example.run
+
+    Geocoder::Lookup::Test.reset
+  end
 
   describe 'geocoding' do
-    let(:payload) {{  'longt' => lng, 'latt' => lat }}
-    let(:result) { [ double(data: payload) ] }
+    subject(:address) { described_class.new lat: nil, lng: nil, full_address: full_address }
 
-    it 'geocodes with Geocoder API' do
-      expect(Geocoder).to receive(:search).with(full_address).and_return result
+    before { address.geocode! }
+
+    it 'fills latitude and longitude' do
+      expect(address.lat).to eq(lat)
+      expect(address.lng).to eq(lng)
     end
 
-    it 'is geocoded' do
+    it 'is considered geocoded' do
       expect(address).to be_geocoded
+    end
+
+  end
+
+  describe '#geocoded?' do
+    context 'when latitude is missing' do
+      let(:lat) { nil }
+
+      it { is_expected.not_to be_geocoded }
+    end
+
+    context 'when longitude is missing' do
+      let(:lng) { nil }
+
+      it { is_expected.not_to be_geocoded }
+    end
+
+    context 'when latitude and longitude are available' do
+      it { is_expected.to be_geocoded }
     end
   end
 
   describe 'reverse geocoding' do
-    let :payload do
-      {   
-        'usa'=> {
-          'uscity' => 'WASHINGTON',
-          'usstnumber' => '1',
-          'state' => 'PA',
-          'zip' => '20500',
-          'usstaddress' => 'Pennsylvania AVE'
-        }
-      }
-    end
-    
-    let(:result) { [ double(data: payload) ] }
+    subject(:address) { described_class.new lat: lat, lng: lng, full_address: nil }
 
-    it 'reverse geocodes with Geocoder API' do
-      expect(Geocoder).to receive(:search).with("#{lat},#{lng}").and_return result
+    before { address.reverse_geocode! }
+
+    it 'fills address' do
+      expect(address.full_address).to eq(full_address)
     end
 
-    it 'is reverse geocoded' do
+    it 'is considered reverse_geocoded' do
       expect(address).to be_reverse_geocoded
     end
   end
 
-  describe 'distance finding' do
-    let(:detroit) { FactoryGirl.build :address, :as_detroit }
-    let(:kansas_city) { FactoryGirl.build :address, :as_kansas_city }
+  describe '#reverse_geocoded?' do
+    context 'when full_address is missing' do
+      let(:full_address) { nil }
 
-    xit 'calculates distance with the Geocoder API' do
-      expect(Geocoder::Calculations).to receive(:distance_between).with detroit.coordinates, kansas_city.coordinates
+      it { is_expected.not_to be_reverse_geocoded }
     end
 
-    xit 'returns the distance between two addresses' do
-      expect(detroit.miles_to(kansas_city)).to be > 0
+    context 'when full_address is present' do
+      it { is_expected.to be_reverse_geocoded }
     end
   end
+
+  # MOVE THIS TO A SERVICE
+  # describe 'distance finding' do
+  #   let(:detroit) { FactoryGirl.build :address, :as_detroit }
+  #   let(:kansas_city) { FactoryGirl.build :address, :as_kansas_city }
+
+  #   xit 'calculates distance with the Geocoder API' do
+  #     expect(Geocoder::Calculations).to receive(:distance_between).with detroit.coordinates, kansas_city.coordinates
+  #   end
+
+  #   xit 'returns the distance between two addresses' do
+  #     expect(detroit.miles_to(kansas_city)).to be > 0
+  #   end
+  # end
 end
